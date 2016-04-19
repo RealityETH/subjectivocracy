3e38d73484532d7d9132d183879dac1b57ebda26
Edmund Edgar <ed@realitykeys.com>
2016-04-19 15:54:12 +0900

    Switch to credit/debit model

--- contracts/bc.sol
+++ contracts/bc.sol
@@ -5,9 +5,11 @@ contract BorgesCoin {
     struct Branch {
         bytes32 parent_hash;
         bytes32 merkle_root;
-        mapping(address => uint256) balances;
+        mapping(address => int256) credits;
+        mapping(address => int256) debits;
     }
     event LogBranch(bytes32 b);
+    event LogBalance(int256 b, string str);
 
     mapping(bytes32 => Branch) branches;
 
@@ -21,53 +23,35 @@ contract BorgesCoin {
         bytes32 null_hash;
         bytes32 genesis_branch_hash = sha3(null_hash, genesis_merkel_root);
         branches[genesis_branch_hash] = Branch(null_hash, genesis_merkel_root);
-        branches[genesis_branch_hash].balances[msg.sender] = 2100000000000000;
+        branches[genesis_branch_hash].credits[msg.sender] = 2100000000000000;
         // LogAddr(msg.sender);
     }
 
-    function sendCoin(address addr, uint256 amount, bytes32 to_branch) returns (bool) {
+    function sendCoin(address addr, int256 amount, bytes32 to_branch) returns (bool) {
         if (amount < 0) {
             return false;
         }
         if (!isBalanceAtLeast(msg.sender, amount, to_branch)) {
             return false;
         }
-        branches[to_branch].balances[msg.sender] = branches[to_branch].balances[msg.sender] - amount;
-        branches[to_branch].balances[addr] = branches[to_branch].balances[addr] + amount;
+        branches[to_branch].debits[msg.sender] += amount;
+        branches[to_branch].credits[addr] += amount;
         return true;
-    }
-
-    function sendCoinFromBranches(address addr, uint256 amount, bytes32 to_branch, bytes32[] from_branches) {
-        var remaining = amount;
-        for(uint256 i=0; i<from_branches.length; i++ ) {
-            bytes32 b = from_branches[i];
-            if (isBranchDescendedFrom(b, to_branch)) {
-                uint256 bal = branches[b].balances[msg.sender];
-                if (bal >= remaining) {
-                    branches[b].balances[msg.sender] = branches[b].balances[msg.sender] - remaining;
-                    remaining = 0;
-                } else {
-                    branches[b].balances[msg.sender] = 0;
-                    remaining = remaining - branches[b].balances[msg.sender];
-                }
-            }
-        }
-        if (remaining == 0) {
-            branches[to_branch].balances[addr] = branches[to_branch].balances[addr] + amount;
-        }
     }
 
     // Crawl up the tree until we get enough
     // This is cheaper than getBalance, which has to go all the way to the root
-    function isBalanceAtLeast(address addr, uint256 min_balance, bytes32 branch_hash) constant returns (bool) {
+    function isBalanceAtLeast(address addr, int256 min_balance, bytes32 branch_hash) constant returns (bool) {
 
-        uint256 bal = 0;
+        // This needs to be signed because we may count debits before we run into credits
+        int256 bal = 0;
 
         bytes32 null_hash;
         while(branch_hash != null_hash) {
-            bal = bal + branches[branch_hash].balances[addr];
+            bal += branches[branch_hash].credits[addr] - branches[branch_hash].debits[addr];
             branch_hash = branches[branch_hash].parent_hash;
             // LogBranch(branch_hash);
+            // LogBalance(bal, "bal so far");
             if (bal >= min_balance) {
                 return true;
             }
@@ -76,13 +60,14 @@ contract BorgesCoin {
 
     }
 
-    function getBalance(address addr, bytes32 branch_hash) constant returns (uint256) {
+    function getBalance(address addr, bytes32 branch_hash) constant returns (int256) {
 
-        uint256 bal = 0;
+        int256 bal = 0;
 
         bytes32 null_hash;
         while(branch_hash != null_hash) {
-            bal = bal + branches[branch_hash].balances[addr];
+            bal = bal + branches[branch_hash].credits[addr] - branches[branch_hash].debits[addr];
+            // LogBalance(bal, "bal so far in getBalance");
             branch_hash = branches[branch_hash].parent_hash;
             // LogBranch(branch_hash);
         }
@@ -95,24 +80,13 @@ contract BorgesCoin {
         bytes32 branch_hash = sha3(parent_b_hash, merkle_root);
         // Only the constructor can create the root branch with no parent
         if (branch_hash == null_hash) {
-            return null_hash;
+            throw;
         }
         branches[branch_hash] = Branch(parent_b_hash, merkle_root);
         return branch_hash;
     }
 
     function requestData(bytes32 branch_hash, bytes32 desired_data_hash) {
-    }
-
-    function isBranchDescendedFrom(bytes32 branch, bytes32 other_branch) returns (bool) {
-        bytes32 parent_hash = branches[branch].parent_hash;
-        while (true) {
-            Branch parent_branch = branches[parent_hash];
-            if (parent_hash == parent_branch.parent_hash) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
--- contracts/test.py
+++ contracts/test.py
@@ -3,6 +3,8 @@ from rlp.utils import encode_hex, decode_hex
 from ethereum import tester as t
 from ethereum import keys
 import time
+from sha3 import sha3_256
+
 
 class TestBorgesCoin(TestCase):
 
@@ -16,22 +18,68 @@ class TestBorgesCoin(TestCase):
 
     def test_register_and_fetch(self):
 
+        # a
+        # aa             ab
+        # aaa  aab       aba
+        # aaaa aaba aabb abaa
+
         genesis_hash = decode_hex("01bd7e296e8be10ff0f93bf1b7186d884f05bdc2c293dbc4ca3ea18a5f7c9ebd")
 
-        bob_addr = encode_hex(keys.privtoaddr(t.k1))
-        self.assertEqual(bob_addr, '7d577a597b2742b498cb5cf0c26cdcd726d39e6e')
+        k0_addr = encode_hex(keys.privtoaddr(t.k0))
+        k1_addr = encode_hex(keys.privtoaddr(t.k1))
+        k2_addr = encode_hex(keys.privtoaddr(t.k2))
+
+        self.assertEqual(k1_addr, '7d577a597b2742b498cb5cf0c26cdcd726d39e6e')
         self.assertEqual(self.bc.getBalance(keys.privtoaddr(t.k0), genesis_hash), 2100000000000000)
 
-        self.bc.sendCoin(bob_addr, 1000000, genesis_hash)
-        self.assertEqual(self.bc.getBalance(keys.privtoaddr(t.k0), genesis_hash), 2100000000000000-1000000)
-        self.assertEqual(self.bc.getBalance(bob_addr, genesis_hash), 1000000)
+        u = self.s.block.gas_used
 
-        dummy_merkle_root = genesis_hash # any bytes32 data will do
-        branch_a1_hash = self.bc.createBranch(genesis_hash, dummy_merkle_root)
-        self.assertEqual(self.bc.getBalance(bob_addr, branch_a1_hash), 1000000)
-        self.assertTrue(self.bc.isBalanceAtLeast(bob_addr, 1000000, branch_a1_hash))
-        self.assertTrue(self.bc.isBalanceAtLeast(bob_addr, 1, branch_a1_hash))
-        self.assertFalse(self.bc.isBalanceAtLeast(bob_addr, 1000001, branch_a1_hash))
+        self.bc.sendCoin(k1_addr, 1000000, genesis_hash, sender=t.k0)
+        # print self.s.block.gas_used - u
+        u = self.s.block.gas_used
+        # print self.s.block.get_balance(k0_addr)
+
+        self.assertEqual(self.bc.getBalance(keys.privtoaddr(t.k0), genesis_hash), 2100000000000000-1000000)
+        self.assertEqual(self.bc.getBalance(k1_addr, genesis_hash), 1000000)
+
+        dummy_merkle_root_aa = decode_hex(sha3_256('aa').hexdigest())
+        dummy_merkle_root_ab = decode_hex(sha3_256('ab').hexdigest())
+
+        dummy_merkle_root_aab = decode_hex(sha3_256('aab').hexdigest())
+        dummy_merkle_root_aba = decode_hex(sha3_256('aba').hexdigest())
+        dummy_merkle_root_abb = decode_hex(sha3_256('abb').hexdigest())
+
+        dummy_merkle_root_aaaa = decode_hex(sha3_256('aaaa').hexdigest())
+        dummy_merkle_root_aaba = decode_hex(sha3_256('aaba').hexdigest())
+
+        branch_aa_hash = self.bc.createBranch(genesis_hash, dummy_merkle_root_aa)
+        branch_ab_hash = self.bc.createBranch(genesis_hash, dummy_merkle_root_ab)
+
+        branch_aab_hash = self.bc.createBranch(branch_aa_hash, dummy_merkle_root_aab)
+        branch_aba_hash = self.bc.createBranch(branch_ab_hash, dummy_merkle_root_aba)
+
+        self.assertEqual(self.bc.getBalance(k1_addr, branch_aa_hash), 1000000)
+
+        self.assertTrue(self.bc.isBalanceAtLeast(k1_addr, 1000000, branch_aa_hash))
+        self.assertTrue(self.bc.isBalanceAtLeast(k1_addr, 1, branch_ab_hash))
+        self.assertFalse(self.bc.isBalanceAtLeast(k1_addr, 1000001, branch_ab_hash))
+
+        u = self.s.block.gas_used
+        self.bc.sendCoin(k2_addr, 500000, branch_aa_hash, sender=t.k1)
+        print "Gas used after %d blocks: %d" % (2, self.s.block.gas_used - u)
+
+        self.assertEqual(self.bc.getBalance(k2_addr, branch_aa_hash), 500000)
+        self.assertEqual(self.bc.getBalance(k2_addr, branch_ab_hash), 0)
+
+        branch_hash = branch_aba_hash
+        for i in range(0,10):
+            dummy_merkel_root = decode_hex(sha3_256('dummy' + str(i)).hexdigest())
+            branch_hash = self.bc.createBranch(branch_hash, dummy_merkel_root)
+            # print encode_hex(branch_hash)
+
+        u = self.s.block.gas_used
+        self.bc.sendCoin(k2_addr, 500000, branch_hash, sender=t.k1)
+        print "Gas used after %d blocks: %d" % (i+1, self.s.block.gas_used - u)
         return
 
          
