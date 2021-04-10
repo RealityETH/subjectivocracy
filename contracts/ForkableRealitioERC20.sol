97e113e938f07df37186cafc2435e2011ff673e9
Edmund Edgar <ed@realitykeys.com>
2021-04-10 00:21:35 +0000

    Remove the ask question version that posts a bounty

--- contracts/ForkableRealitioERC20.sol
+++ contracts/ForkableRealitioERC20.sol
@@ -263,33 +263,6 @@ contract RealitioERC20_v2_1 is BalanceHolder {
         return question_id;
     }
 
-    /// @notice Ask a new question with a bounty and return the ID
-    /// @dev Template data is only stored in the event logs, but its block number is kept in contract storage.
-    /// @param template_id The ID number of the template the question will use
-    /// @param question A string containing the parameters that will be passed into the template to make the question
-    /// @param arbitrator The arbitration contract that will have the final word on the answer if there is a dispute
-    /// @param timeout How long the contract should wait after the answer is changed before finalizing on that answer
-    /// @param opening_ts If set, the earliest time it should be possible to answer the question.
-    /// @param nonce A user-specified nonce used in the question ID. Change it to repeat a question.
-    /// @param tokens The combined initial question bounty and question fee
-    /// @return The ID of the newly-created question, created deterministically.
-    function askQuestionERC20(uint256 template_id, string question, address arbitrator, uint32 timeout, uint32 opening_ts, uint256 nonce, uint256 tokens) 
-        // stateNotCreated is enforced by the internal _askQuestion
-    public returns (bytes32) {
-
-        _deductTokensOrRevert(tokens);
-
-        require(templates[template_id] > 0, "template must exist");
-
-        bytes32 content_hash = keccak256(abi.encodePacked(template_id, opening_ts, question));
-        bytes32 question_id = keccak256(abi.encodePacked(content_hash, arbitrator, timeout, msg.sender, nonce));
-
-        _askQuestion(question_id, content_hash, arbitrator, timeout, opening_ts, tokens);
-        emit LogNewQuestion(question_id, msg.sender, template_id, question, content_hash, arbitrator, timeout, opening_ts, nonce, now);
-
-        return question_id;
-    }
-
     function _deductTokensOrRevert(uint256 tokens) 
     internal {
 
