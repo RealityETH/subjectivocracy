0a686f26d9a0bc683ebeacad600a9fed2042206b
Edmund Edgar <ed@realitykeys.com>
2021-04-28 00:26:24 +0000

    Revert "Remove method we don't use"
    This reverts commit 7836cc814be9b06f2d1d4356616f0bd8ab260b24.

--- contracts/ForkableRealitioERC20.sol
+++ contracts/ForkableRealitioERC20.sol
@@ -350,6 +350,27 @@ contract ForkableRealitioERC20 is BalanceHolderERC20 {
         emit LogNotifyOfArbitrationRequest(question_id, requester);
     }
 
+
+    /// @notice Submit the answer for a question, for use by the arbitrator.
+    /// @dev Doesn't require (or allow) a bond.
+    /// If the current final answer is correct, the account should be whoever submitted it.
+    /// If the current final answer is wrong, the account should be whoever paid for arbitration.
+    /// However, the answerer stipulations are not enforced by the contract.
+    /// @param question_id The ID of the question
+    /// @param answer The answer, encoded into bytes32
+    /// @param answerer The account credited with this answer for the purpose of bond claims
+    function submitAnswerByArbitrator(bytes32 question_id, bytes32 answer, address answerer) 
+        onlyArbitrator(question_id)
+        statePendingArbitration(question_id)
+    public {
+        require(answerer != NULL_ADDRESS, "answerer must be provided");
+        emit LogFinalize(question_id, answer);
+
+        questions[question_id].is_pending_arbitration = false;
+        _addAnswerToHistory(question_id, answer, answerer, 0);
+        _updateCurrentAnswerByArbitrator(question_id, answer);
+    }
+
     /// @notice Submit the answer for a question, for use by the arbitrator, working out the appropriate winner based on the last answer details.
     /// @dev Doesn't require (or allow) a bond.
     /// @param question_id The ID of the question
