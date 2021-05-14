15f35ce5a1d4bbd1cc85f6c3789bbff3f209f6ba
Edmund Edgar <ed@socialminds.jp>
2021-05-14 09:14:44 +0900

    Add mint

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -11,6 +11,8 @@ interface IERC20 {
 
     function allowance(address owner, address spender) external view returns (uint256);
 
+    function mint(address to, uint256 value) external returns (bool);
+
     function transfer(address to, uint256 value) external returns (bool);
 
     function approve(address spender, uint256 value) external returns (bool);
