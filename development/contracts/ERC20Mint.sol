105fdd287f94a9186fec2a0dd04aa97baf8a723c
Edmund Edgar <ed@socialminds.jp>
2022-01-19 01:52:10 +0000

    Add an ERC20 token with the ability to mint

---
+++ development/contracts/ERC20Mint.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.10;
+
+import './ERC20.sol';
+import './IERC20Mint.sol';
+
+contract ERC20Mint is ERC20 {
+
+    function mint(address to, uint256 value) external {
+        balanceOf[to] = balanceOf[to] + value;
+    }
+
+}
