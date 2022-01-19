c32f9d9c9186c867d2c7ac2816f64cee69f1c30e
Edmund Edgar <ed@socialminds.jp>
2022-01-19 02:16:05 +0000

    Make a stub AMB contract for testing

---
+++ development/contracts/AMB.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity ^0.8.10;
+
+import './IAMB.sol';
+
+contract AMB is IAMB {
+    function requireToPassMessage(
+        address _contract,
+        bytes memory _data,
+        uint256 _gas
+    ) external returns (bytes32) {
+
+    }
+
+    function maxGasPerTx() external view returns (uint256) {
+
+    }
+
+    function messageSender() external view returns (address) {
+
+    }
+
+    function messageSourceChainId() external view returns (bytes32) {
+
+    }
+
+    function messageId() external view returns (bytes32) {
+
+    }
+}
