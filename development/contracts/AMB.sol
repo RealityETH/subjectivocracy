a4ce8cf9f200502aac04b972e1c88a48e1f1c1ce
Edmund Edgar <ed@socialminds.jp>
2022-03-01 04:59:11 +0000

    Add a bit more dummy implementation for the bridge so we can test making calls through it

--- development/contracts/AMB.sol
+++ development/contracts/AMB.sol
@@ -5,6 +5,11 @@ pragma solidity ^0.8.10;
 import './IAMB.sol';
 
 contract AMB is IAMB {
+
+    address sender;
+    bytes32 sourceChainId;
+    bytes32 public messageId;
+
     function requireToPassMessage(
         address _contract,
         bytes memory _data,
@@ -13,19 +18,61 @@ contract AMB is IAMB {
 
     }
 
-    function maxGasPerTx() external view returns (uint256) {
+    function maxGasPerTx() public view returns (uint256) {
 
     }
 
-    function messageSender() external view returns (address) {
-
+    function messageSender() public view returns (address) {
+        return sender;
     }
 
-    function messageSourceChainId() external view returns (bytes32) {
-
+    function messageSourceChainId() public view returns (bytes32) {
+        return sourceChainId;
     }
 
-    function messageId() external view returns (bytes32) {
+    // Stripped-down simulated message passing, from:
+    // https://github.com/poanetwork/tokenbridge-contracts/blob/c9377114f7bcf04cd12a30d9eca0a63362dcaedc/contracts/upgradeable_contracts/arbitrary_message/MessageProcessor.sol#L211
 
+    /**
+    * @dev Makes a call to the message executor.
+    * @param _sender sender address on the other side.
+    * @param _contract address of an executor contract.
+    * @param _data calldata for a call to executor.
+    * @param _gas gas limit for a call to executor. 2^32 - 1, if caller will pass all available gas for the execution.
+    * @param _messageId id of the processed message.
+    * @param _sourceChainId source chain id is of the received message.
+    */
+    function passMessage(
+        address _sender,
+        address _contract,
+        bytes memory _data,
+        uint256 _gas,
+        bytes32 _messageId,
+        bytes32 _sourceChainId
+    ) external returns (bool) {
+        sender = _sender;
+        messageId = _messageId;
+        sourceChainId = _sourceChainId;
+
+        // After EIP-150, max gas cost allowed to be passed to the internal call is equal to the 63/64 of total gas left.
+        // In reality, min(gasLimit, 63/64 * gasleft()) will be used as the call gas limit.
+        // Imagine a situation, when message requires 10000000 gas to be executed successfully.
+        // Also suppose, that at this point, gasleft() is equal to 10158000, so the callee will receive ~ 10158000 * 63 / 64 = 9999300 gas.
+        // That amount of gas is not enough, so the call will fail. At the same time,
+        // even if the callee failed the bridge contract still has ~ 158000 gas to
+        // finish its execution and it will be enough. The internal call fails but
+        // only because the oracle provides incorrect gas limit for the transaction
+        // This check is needed here in order to force contract to pass exactly the requested amount of gas.
+        // Avoiding it may lead to the unwanted message failure in some extreme cases.
+        require(_gas == 0xffffffff || (gasleft() * 63) / 64 > _gas);
+
+        (bool status, ) = _contract.call{gas: _gas}(_data);
+        // _validateExecutionStatus(status);
+
+        sender = address(0);
+        messageId = bytes32(0x0);
+        sourceChainId = 0;
+        return status;
     }
+
 }
