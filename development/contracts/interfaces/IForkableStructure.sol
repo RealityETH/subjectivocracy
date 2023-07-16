e21bd50fc0072fbafdd863d5bd3dc326f5cb7bdd
josojo <josojo@hotmail.de>
2023-07-16 23:22:42 +0200

    Revert "forking strcuture that will be inherited by all other contracts (#44)"
    This reverts commit c95c1ccf07201772e298decc7310f012e8149791.

--- development/contracts/interfaces/IForkableStructure.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.8.17;
-
-interface IForkableStructure {
-    function getChild(uint256 index) external view returns (address);
-
-    function getChildren() external view returns (address, address);
-}
