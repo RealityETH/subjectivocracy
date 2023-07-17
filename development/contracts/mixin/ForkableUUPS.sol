ffba6d25e02dfca2df5f6d96179ecce75228f7ea
josojo <josojo@hotmail.de>
2023-07-17 08:22:30 +0200

    starting

---
+++ development/contracts/mixin/ForkableUUPS.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "./ForkStructure.sol";
+
+abstract contract ForkableUUPS is
+    ForkStructure,
+    UUPSUpgradeable,
+    AccessControlUpgradeable
+{
+    bytes32 public constant UPDATER = keccak256("UPDATER");
+
+    function initialize(address _forkmanager, address _parentContract) public virtual override(ForkStructure) onlyInitializing {
+        ForkStructure.initialize(_forkmanager, _parentContract);
+    }
+
+    function _authorizeUpgrade(address) internal view override {
+        require(hasRole(UPDATER, msg.sender));
+    }
+
+    function _createChildren(
+        address implementation
+    ) internal returns (address forkingManager1, address forkingManager2) {
+        forkingManager1 = address(new ERC1967Proxy(_getImplementation(), ""));
+        children[0] = forkingManager1;
+        forkingManager2 = address(new ERC1967Proxy(implementation, ""));
+        children[1] = forkingManager2;
+    }
+}
