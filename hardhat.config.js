0ed5021a006fdb12874957dd1855ac26c6db405a
josojo <josojo@hotmail.de>
2023-08-20 13:03:28 +0200

    change solidity version for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -58,7 +58,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.20",
         settings: {
           optimizer: {
             enabled: true,
