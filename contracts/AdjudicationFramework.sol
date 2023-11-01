1d31742c16799d0630056b23c43b0e51cba18a21
Edmund Edgar <ed@socialminds.jp>
2023-11-01 06:13:22 +0000

    comment about dispute fee

--- contracts/AdjudicationFramework.sol
+++ contracts/AdjudicationFramework.sol
@@ -135,7 +135,7 @@ contract AdjudicationFramework is BalanceHolder {
     /// @notice Return the dispute fee for the specified question. 0 indicates that we won't arbitrate it.
     /// @dev Uses a general default, but can be over-ridden on a question-by-question basis.
     function getDisputeFee(bytes32) public view returns (uint256) {
-        // Todo: make it depend on the question
+        // TODO: Should we have a governance process to change this?
         return dispute_fee;
     }
 
