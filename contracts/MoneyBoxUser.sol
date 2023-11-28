0296ab432a0675f7335998eb47198f13d03277da
Edmund Edgar <ed@socialminds.jp>
2023-11-28 02:59:26 +0000

    Use the openzeppelin createCode function instead of copy-pasting stuff from the solc docs

--- contracts/MoneyBoxUser.sol
+++ contracts/MoneyBoxUser.sol
@@ -3,22 +3,20 @@
 pragma solidity ^0.8.20;
 
 import {MoneyBox} from "./MoneyBox.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
 abstract contract MoneyBoxUser {
 
     function _calculateMoneyBoxAddress(address _creator, bytes32 _salt, address _token) internal pure returns (address) {
 
-        // From:
-        // https://docs.soliditylang.org/en/latest/control-structures.html#salted-contract-creations-create2
-        return address(uint160(uint(keccak256(abi.encodePacked(
-            bytes1(0xff),
-            _creator,
-            _salt,
+        return Create2.computeAddress(
+            _salt, 
             keccak256(abi.encodePacked(
                 type(MoneyBox).creationCode,
-                abi.encode(_token) 
-            ))
-        )))));
+                abi.encode(_token)
+            )),
+            _creator
+        );
 
     }
 
