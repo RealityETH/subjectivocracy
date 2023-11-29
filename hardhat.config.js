fcc63bfb46060177a22bd2447b348dd2520651ff
josojo <josojo@hotmail.de>
2023-11-29 12:53:29 +0100

    adding sepolia chain

--- hardhat.config.js
+++ hardhat.config.js
@@ -123,6 +123,15 @@ module.exports = {
         count: 20,
       },
     },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+      },
+    },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: {
