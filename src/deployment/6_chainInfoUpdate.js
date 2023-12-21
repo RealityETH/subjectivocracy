fd549efdbaf6f734888194e7235e29300f9b0de2
Edmund Edgar <ed@socialminds.jp>
2023-12-21 05:44:13 +0000

    Refactor to reduce code duplication

--- src/deployment/6_chainInfoUpdate.js
+++ src/deployment/6_chainInfoUpdate.js
@@ -5,6 +5,8 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const { ethers } = require('hardhat');
 
+const deployParameters = require('./deploy_application_parameters.json');
+
 const pathOutputJsonL1System = path.join(__dirname, './deploy_output.json');
 const pathOutputJsonL1Applications = path.join(__dirname, './deploy_output_l1_applications.json');
 const pathOutputJsonL2Applications = path.join(__dirname, './deploy_output_l2_applications.json');
@@ -13,19 +15,14 @@ const l1Applications = require(pathOutputJsonL1Applications);
 const l2Applications = require(pathOutputJsonL2Applications);
 const l1SystemAddresses = require(pathOutputJsonL1System);
 
+const common = require('./common.js');
+
 async function main() {
 
-    const currentProvider = ethers.provider;
-    let deployer;
-    if (process.env.PVTKEY) {
-        deployer = new ethers.Wallet(process.env.PVTKEY, currentProvider);
-        console.log('Using pvtKey deployer with address: ', deployer.address);
-    } else if (process.env.MNEMONIC) {
-        deployer = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, 'm/44\'/60\'/0\'/0/0').connect(currentProvider);
-        console.log('Using MNEMONIC deployer with address: ', deployer.address);
-    } else {
-        [deployer] = (await ethers.getSigners());
-    }
+    const deployParameters = require('./deploy_application_parameters.json');
+
+    let currentProvider = await common.loadProvider(deployParameters, process.env);
+    let deployer = await common.loadDeployer(currentProvider, deployParameters);
 
     const l1BridgeAddress = l1SystemAddresses.polygonZkEVMBridgeAddress;
 
---
+++ src/deployment/common.js
@@ -0,0 +1,114 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if, import/no-dynamic-require, global-require */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved, no-restricted-syntax */
+const path = require('path');
+const fs = require('fs');
+const { ethers, upgrades } = require('hardhat');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const pathGenesisJson = path.join(__dirname, './genesis.json');
+
+async function loadProvider(deployParameters = {}, env) {
+
+    let currentProvider = ethers.provider;
+    if (deployParameters.multiplierGas || deployParameters.maxFeePerGas) {
+        if (env.HARDHAT_NETWORK !== 'hardhat') {
+            currentProvider = new ethers.providers.JsonRpcProvider(`https://${env.HARDHAT_NETWORK}.infura.io/v3/${env.INFURA_PROJECT_ID}`);
+            if (deployParameters.maxPriorityFeePerGas && deployParameters.maxFeePerGas) {
+                console.log(`Hardcoded gas used: MaxPriority${deployParameters.maxPriorityFeePerGas} gwei, MaxFee${deployParameters.maxFeePerGas} gwei`);
+                const FEE_DATA = {
+                    maxFeePerGas: ethers.utils.parseUnits(deployParameters.maxFeePerGas, 'gwei'),
+                    maxPriorityFeePerGas: ethers.utils.parseUnits(deployParameters.maxPriorityFeePerGas, 'gwei'),
+                };
+                currentProvider.getFeeData = async () => FEE_DATA;
+            } else {
+                console.log('Multiplier gas used: ', deployParameters.multiplierGas);
+                async function overrideFeeData() {
+                    const feedata = await ethers.provider.getFeeData();
+                    return {
+                        maxFeePerGas: feedata.maxFeePerGas.mul(deployParameters.multiplierGas).div(1000),
+                        maxPriorityFeePerGas: feedata.maxPriorityFeePerGas.mul(deployParameters.multiplierGas).div(1000),
+                    };
+                }
+                currentProvider.getFeeData = overrideFeeData;
+            }
+        }
+    }
+    return currentProvider;
+
+};
+
+async function loadDeployer(currentProvider, deployParameters = {}, idx = '0') {
+
+    // Load deployer
+    let deployer;
+    if (deployParameters.deployerPvtKey) {
+        if (idx > 0) {
+            throw new Error('Multiple signers only supported with mnemonic');
+        }
+        deployer = new ethers.Wallet(deployParameters.deployerPvtKey, currentProvider);
+        console.log('Using pvtKey deployer with address: ', deployer.address);
+    } else if (process.env.MNEMONIC) {
+        deployer = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, 'm/44\'/60\'/0\'/0/'+idx).connect(currentProvider);
+        console.log('Using MNEMONIC deployer with address: ', deployer.address);
+    } else {
+        if (idx > 0) {
+            throw new Error('Multiple signers only supported with mnemonic');
+        }
+        [deployer] = (await ethers.getSigners());
+    }
+    return deployer;
+}
+
+function verifyDeploymentParameters(mandatoryDeploymentParameters, deployParameters) {
+    for (const parameterName of mandatoryDeploymentParameters) {
+        if (deployParameters[parameterName] === undefined || deployParameters[parameterName] === '') {
+            throw new Error(`Missing parameter: ${parameterName}`);
+        }
+    }
+};
+
+async function loadOngoingOrDeploy(deployer, contractName, ongoingName, args, ongoingDeploymentEntries, pathOngoingDeploymentJson, externallyDeployedAddress) {
+
+    const contractFactory = await ethers.getContractFactory(contractName, {
+        signer: deployer,
+    });
+
+    let existingAddress;
+    if (externallyDeployedAddress) {
+        existingAddress = externallyDeployedAddress;
+    } else if (ongoingDeploymentEntries[ongoingName]) {
+        existingAddress = ongoingDeploymentEntries[ongoingName];
+    }
+
+    let contractInstance;
+    if (!existingAddress) {
+        contractInstance = await contractFactory.deploy(...args);
+        console.log('#######################\n');
+        console.log(ongoingName, 'deployed to:', contractInstance.address);
+
+        // save an ongoing deployment
+        ongoingDeploymentEntries[ongoingName] = contractInstance.address;
+        fs.writeFileSync(pathOngoingDeploymentJson, JSON.stringify(ongoingDeploymentEntries, null, 1));
+    } else {
+        contractInstance = contractFactory.attach(existingAddress);
+        console.log('#######################\n');
+        console.log(ongoingName, 'already deployed on: ', existingAddress);
+    }
+
+    return contractInstance;
+
+};
+
+function genesisAddressForContractName(contractName) {
+    const genesisJSON = require(pathGenesisJson);
+    const genesisEntries = genesisJSON.genesis;
+    for(const genesisIdx in genesisEntries) {
+        const genesisEntry = genesisEntries[genesisIdx];
+        if (genesisEntry.contractName == contractName) {
+            return genesisEntry.address;
+        }
+    }
+    throw new Error("Could not find genesis entry "+contractName);
+}
+
+module.exports = { verifyDeploymentParameters, loadProvider, loadDeployer, loadOngoingOrDeploy, genesisAddressForContractName };
