976de07a239454ac787379a691119238040ff3fb
josojo <josojo@hotmail.de>
2024-01-08 20:35:58 +0100

    test

--- test/AdjudicationFramework/IterableList.t.sol
+++ test/AdjudicationFramework/IterableList.t.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
-import {IterableListWrapper} from "../testContract/IterableListWrapper.sol";
+import {IterableListWrapper} from "./../testContract/IterableListWrapper.sol";
 
 contract IterableListTest is Test {
     IterableListWrapper public iterableList;
