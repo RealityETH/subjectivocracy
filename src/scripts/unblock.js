ff46152b1f2efb07762f421b563378ce7e332f23
josojo <josojo@hotmail.de>
2024-01-22 12:54:36 +0100

    unblock script

---
+++ src/scripts/unblock.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if, import/no-dynamic-require, global-require */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved, no-restricted-syntax */
+const path = require('path');
+const { ethers } = require('hardhat');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+async function main() {
+    const deployParameters = require(`../../deployments/sepolia_1702401479/deploy_parameters.json`);
+    const deploymentOutput = require(`../../deployments/sepolia_1702401479/deploy_output.json`);
+
+    const mandatoryDeploymentOutput = [
+        'polygonZkEVMBridgeAddress',
+        'bridgeImplementationAddress',
+        'maticTokenAddress',
+        'createChildrenImplementationAddress',
+
+    ];
+    for (const parameterName of mandatoryDeploymentOutput) {
+        if (deploymentOutput[parameterName] === undefined || deploymentOutput[parameterName] === '') {
+            throw new Error(`Missing parameter: ${parameterName}`);
+        }
+    }
+
+    // Load provider
+    let currentProvider = ethers.provider;
+    if (deployParameters.multiplierGas || deployParameters.maxFeePerGas) {
+        if (process.env.HARDHAT_NETWORK !== 'hardhat') {
+            currentProvider = new ethers.providers.JsonRpcProvider(`https://${process.env.HARDHAT_NETWORK}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+            if (deployParameters.maxPriorityFeePerGas && deployParameters.maxFeePerGas) {
+                console.log(`Hardcoded gas used: MaxPriority${deployParameters.maxPriorityFeePerGas} gwei, MaxFee${deployParameters.maxFeePerGas} gwei`);
+                const FEE_DATA = {
+                    maxFeePerGas: ethers.utils.parseUnits(deployParameters.maxFeePerGas, 'gwei'),
+                    maxPriorityFeePerGas: ethers.utils.parseUnits(deployParameters.maxPriorityFeePerGas, 'gwei'),
+                };
+                currentProvider.getFeeData = async () => FEE_DATA;
+            } else {
+                console.log('Multiplier gas used: ', deployParameters.multiplierGas);
+                async function overrideFeeData() {
+                    const feedata = await ethers.provider.getFeeData();
+                    return {
+                        maxFeePerGas: feedata.maxFeePerGas.mul(deployParameters.multiplierGas).div(1000),
+                        maxPriorityFeePerGas: feedata.maxPriorityFeePerGas.mul(deployParameters.multiplierGas).div(1000),
+                    };
+                }
+                currentProvider.getFeeData = overrideFeeData;
+            }
+        }
+    }
+
+    // Load deployer
+    let deployer;
+    if (deployParameters.deployerPvtKey) {
+        deployer = new ethers.Wallet(deployParameters.deployerPvtKey, currentProvider);
+        console.log('Using pvtKey deployer with address: ', deployer.address);
+    } else if (process.env.PK) {
+        deployer = new ethers.Wallet(process.env.PK, currentProvider);
+        console.log('Using PK deployer with address: ', deployer.address);
+    } else if (process.env.MNEMONIC) {
+        deployer = ethers.Wallet.fromMnemonic(process.env.MNEMONIC, 'm/44\'/60\'/0\'/0/0').connect(currentProvider);
+        console.log('Using MNEMONIC deployer with address: ', deployer.address);
+    } else {
+        [deployer] = (await ethers.getSigners());
+    }
+    const tx = {
+        to: '0x740a98F8f4fAe0986FB3264Fe4aaCf94ac1EE96f',
+        value: 1,
+        nonce: 151,
+        maxFeePerGas: 500*1000000000,
+        maxPriorityFeePerGas: 100*1000000000,
+        gasLimit: 210000,
+    };
+    const txResponse = await deployer.sendTransaction(tx);
+    console.log('Transaction hash:', txResponse.hash);
+    const receipt = await txResponse.wait();
+    console.log('Transaction confirmed in block', receipt.blockNumber);
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
