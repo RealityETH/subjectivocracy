3a6eff405f4a44e48d70dd600ba68317bc9e0543
josojo <josojo@hotmail.de>
2024-02-07 13:18:35 +0100

    cutting out the core functionality

--- contracts/lib/reality-eth/interfaces/IOwned.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-
-pragma solidity ^0.8.10;
-
-interface IOwned {
-    function owner() external view returns (address);
-
-    function transferOwnership(address newOwner) external;
-}
