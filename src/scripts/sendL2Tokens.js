0bfcd3eb40b1b7d2cb9f3f6c6c22e9a6ddbf3650
Edmund Edgar <ed@socialminds.jp>
2024-04-12 08:27:01 +0000

    script to send tokens on l2 from deployer to specified address

---
+++ src/scripts/sendL2Tokens.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-await-in-loop, no-use-before-define, no-lonely-if, import/no-dynamic-require, global-require */
+/* eslint-disable no-console, no-inner-declarations, no-undef, import/no-unresolved, no-restricted-syntax */
+const path = require('path');
+const { ethers } = require('hardhat');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const common = require('../common/common');
+
+async function main() {
+
+    const args = process.argv.slice(2);
+    const deploymentName = args[0];
+    const recipient = args[1];
+    const amount = ethers.BigNumber.from(args[2]);
+
+    const deployParameters = require(`../../deployments/${deploymentName}/deploy_parameters.json`);
+    const deploymentOutput = require(`../../deployments/${deploymentName}/deploy_output.json`);
+
+    const currentProvider = await common.loadProvider(deployParameters, process.env);
+    const deployer = await common.loadDeployer(currentProvider, deployParameters);
+
+    const bal = await currentProvider.getBalance(deployer.address)
+    const gas_price = await currentProvider.getGasPrice()
+    const nonce = await currentProvider.getTransactionCount(deployer.address, 'latest')
+    const gas_limit = 21000;
+
+    const txdata = {
+      to: recipient,
+      value: amount,
+      nonce: nonce,
+      gasLimit: ethers.utils.hexlify(gas_limit), 
+      gasPrice: gas_price,
+    }
+
+    const response = await deployer.sendTransaction(txdata);
+    console.log('sent tx', response.hash)
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
